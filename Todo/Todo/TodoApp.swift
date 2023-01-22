5430b15826d33a6d83ba1a4098d1c216031a3e07
Nikoloutsos <konstantinosnikolouts@gmail.com>
2023-01-23 00:50:52 +0200

    Scaffold initial demo app

---
+++ Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Konstantinos Nikoloutsos on 23/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
